f872de4ccfa6e4a30180372d58c9da3c7268302e
HaxagonusD <jquezad1@binghamton.edu>
2021-09-02 03:13:30 -0400

    searching, parsing, filtering works. Added todo list 20% complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 # response = requests.get('https://httpbin.org/ip')
@@ -33,18 +34,53 @@ def search_one_tax_form(tax_form_name):
 # you get some html and you get back some useful objects
 def parse_one_tax_form_search_data(tax_form_search_data):
     soup = BeautifulSoup(tax_form_search_data, "html.parser")
-   
+
     form_objects = []
     table_rows_list = soup.find_all("tr", class_=True)
-    print(table_rows_list)
-    print(len(table_rows_list))
+    # print(table_rows_list[0].contents[1].contents[1].string)
+    for table_row in table_rows_list:
+        form_objects.append(
+            {
+                "form_number": re.sub(
+                    "\s+", " ", table_row.contents[1].contents[1].string
+                ).strip(),
+                "form_title": re.sub("\s+", " ", table_row.contents[3].string).strip(),
+                "year": re.sub("\s+", " ", table_row.contents[5].string).strip(),
+            }
+        )
+    return form_objects
+
+def filter_data_by_tax_form(parsed_data, tax_form_name):
+    def is_exact_tax_form_name(data):
+        return True if data["form_number"] == tax_form_name else False 
+    
+    filtered_data = filter(is_exact_tax_form_name, parsed_data)
+    for tax_form in filtered_data:
+        print(tax_form)
+    
+   
+
+
     
 
 
 form_w2_data = search_one_tax_form("form w2")
-parse_one_tax_form_search_data(form_w2_data)
+parsed_data = parse_one_tax_form_search_data(form_w2_data)
+filter_data_by_tax_form(parsed_data, "Form W-2")
 
 
 # then given some useful objects
 # you either have a function that gives spits out json in some format based on the object
 # or you download a bunch of files to some directory based of the object
+
+
+#TODO
+#Get all the information on every page by searching, and parsing, and filtering 
+#The above is based on either what apge youa re on or how many entries are left in the table 
+
+#After that I need to be able to download into a certain directory. shouldn't be that hard. I just need path and a pdf downloader library 
+
+# then I need to be able to get the min and max data for one  for one form
+#so that I can do it for multiple forms. 
+
+#After that I need to make it all worth with the commandline 
