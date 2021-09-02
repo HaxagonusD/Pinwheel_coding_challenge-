0dd5edcb9b6c39e8e7371d6352359ee58e3033a4
HaxagonusD <jquezad1@binghamton.edu>
2021-09-02 00:42:08 -0400

    Relearning python

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests 
+
+# response = requests.get('https://httpbin.org/ip')
+
+# print('Your IP is {0}'.format(response.json()['origin']))
+
+# this needs search 
+# you give a fucntion some paramenters and you get back some html 
+
+
+
+#Then it needs a a parser
+# you get some html and you get back some useful objects 
+
+# then given some useful objects 
+# you either have a function that gives spits out json in some format based on the object 
+# or you download a bunch of files to some directory based of the object
